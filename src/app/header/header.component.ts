7207b36cfd6a3eab9444cef434db951752d9cd77
jarog <jgronost@gmail.com>
2018-02-23 16:40:51 +0100

    get recipes from firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,4 +19,7 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  onFetchData() {
+    this.dataStorage.getRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe';
 
 @Injectable()
 export class DataStorageService {
+  private recipesUrl = 'https://jgronost-udemy-recipe-book.firebaseio.com/recipes.json';
 
   constructor(private http: Http, private recipeService: RecipeService) { }
 
   storeRecipes() {
-    return this.http.put('https://jgronost-udemy-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    return this.http.put(this.recipesUrl, this.recipeService.getRecipes());
+  }
+
+  getRecipes() {
+    this.http.get(this.recipesUrl)
+      .subscribe(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json(); 
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 
 }
