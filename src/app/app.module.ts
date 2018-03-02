7a2afaa08e496dc02373170d71c85b1b6602a7d3
jarog <jgronost@gmail.com>
2018-03-02 21:04:20 +0100

    provide services  from core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,8 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth.guard';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 
@@ -31,12 +26,7 @@ import { CoreModule } from './core/core.module';
     AuthModule,
     CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
