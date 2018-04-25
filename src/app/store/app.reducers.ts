75f88299fdd9b80457dfd8f636731a0def4cc36f
Jaroslaw Gronostajski <jaroslaw,gronostajski@kwsoft.com>
2018-04-25 15:36:49 +0200

    correct quotes in imports

--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,14 +1,15 @@
-import { ShoppingListState, shoppingListReducer } from "../shopping-list/store/shopping-list.reducers";
-import { AuthState, authReducers } from "../auth/store/auth.reducers";
-import { ActionReducerMap } from "@ngrx/store";
+import { ShoppingListState, shoppingListReducer } from '../shopping-list/store/shopping-list.reducers';
+import { AuthState, authReducers } from '../auth/store/auth.reducers';
+import { ActionReducerMap } from '@ngrx/store';
 
 
 export interface AppState {
-    shoppingList: ShoppingListState,
-    auth: AuthState
+    shoppingList: ShoppingListState;
+    auth: AuthState;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
     shoppingList: shoppingListReducer,
     auth: authReducers
 }
+
